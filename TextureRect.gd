909599f2461dfd62277691c63d1a0667a0b59bb7
Леонид Харлов <harlov.leonid@ya.ru>
2024-07-04 20:14:42 +0500

    change variable names

--- TextureRect.gd
+++ TextureRect.gd
@@ -9,12 +9,12 @@ extends TextureRect
 
 # SHADER VARIABLES
 @export_file("*.glsl") var shader_path : String = "res://light_adjustments.glsl"
-var texture_compute_shader : ComputeHelper
-var texture_shader_groups : Vector3i
+var compute_shader : ComputeHelper
+var shader_groups : Vector3i
 var input_texture : ImageUniform
 var output_texture : ImageUniform
-var texture_parameters_buffer : StorageBufferUniform
-var shader_params = [0.0, 0.0]
+var shader_parameters_buffer : StorageBufferUniform
+var shader_parameters = [0.0, 0.0]
 
 func _ready():
 	# to safely access rendering internals, it is better to call functions this way
@@ -29,7 +29,7 @@ func init_shader():
 	texture = Texture2DRD.new()
 	
 	# init shader and shader pipeline
-	texture_compute_shader = ComputeHelper.create(shader_path)
+	compute_shader = ComputeHelper.create(shader_path)
 	
 	# init image uniforms
 	input_texture = ImageUniform.create(image)
@@ -42,31 +42,31 @@ func init_shader():
 	var x_groups = (image_size.x - 1) / 8 + 1
 	var y_groups = (image_size.y - 1) / 8 + 1
 	var z_groups = 1
-	texture_shader_groups = Vector3i(x_groups, y_groups, z_groups)
+	shader_groups = Vector3i(x_groups, y_groups, z_groups)
 	
 	# init empty buffer for shader parameters
-	texture_parameters_buffer = StorageBufferUniform.create(PackedFloat32Array([0.0]).to_byte_array())
+	shader_parameters_buffer = StorageBufferUniform.create(PackedFloat32Array([0.0]).to_byte_array())
 	
 	# add uniforms to shader pipeline
-	texture_compute_shader.add_uniform_array([texture_parameters_buffer, input_texture, output_texture])
+	compute_shader.add_uniform_array([shader_parameters_buffer, input_texture, output_texture])
 
 
-func update_shader(shader_params):
-	# send shader_params to shader
-	texture_parameters_buffer.update_data(PackedFloat32Array(shader_params).to_byte_array())
+func update_shader(shader_parameters):
+	# send shader_parameters to shader
+	shader_parameters_buffer.update_data(PackedFloat32Array(shader_parameters).to_byte_array())
 	# execute shader
-	texture_compute_shader.run(texture_shader_groups)
+	compute_shader.run(shader_groups)
 
 
 func _on_brightness_slider_value_changed(value):
-	# change first value in shader_params array (brightness)
-	shader_params[0] = value
+	# change first value in shader_parameters array (brightness)
+	shader_parameters[0] = value
 	# execute shader
-	update_shader(shader_params)
+	update_shader(shader_parameters)
 
 
 func _on_contrast_slider_value_changed(value):
-	# change second value in shader_params array (contrast)
-	shader_params[1] = value
+	# change second value in shader_parameters array (contrast)
+	shader_parameters[1] = value
 	# execute shader
-	update_shader(shader_params)
+	update_shader(shader_parameters)
